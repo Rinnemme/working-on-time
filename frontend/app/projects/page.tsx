8431f01ab6fd50c4f8c1dc3efbcb7b461a476ed8
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-01-15 11:54:23 -0500

    incorporate created and due dates into project-item

--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -8,12 +8,12 @@ export default function Projects() {
 
   return (
     <div className="bg-white top-0 w-full h-full z-0 fade-in">
-      <div className="relative flex items-start pt-12 sm:pt-14 justify-center w-full bg-white">
+      <div className="relative flex items-start pt-12 mb-20 sm:pt-14 justify-center w-full bg-white">
         <div className="text-center flex items-center w-full flex-col px-2">
           <h1 className="text-2xl font-extrabold text-wot-rose sm:text-3xl">
             Your Projects
           </h1>
-          <div className="mt-10 flex flex-col gap-4 w-full max-w-lg">
+          <div className="mt-10 flex flex-col gap-4 w-full max-w-lg px-3">
             {projects.map((project) => {
               return (
                 <ProjectItem key={`project-${project.id}`} project={project} />
--- frontend/src/components/project-item/project-item.tsx
+++ frontend/src/components/project-item/project-item.tsx
@@ -2,6 +2,7 @@
 import type { Project } from "@/src/store/types";
 
 export default function ProjectItem({ project }: { project: Project }) {
+  console.log(project);
   const priority =
     project.priority === 1
       ? { textColor: "text-wot-blue", string: "Low" }
@@ -9,29 +10,41 @@ export default function ProjectItem({ project }: { project: Project }) {
       ? { textColor: "text-wot-green", string: "Medium" }
       : { textColor: "text-wot-yellow", string: "High" };
   return (
-    <div className="w-full flex flex-col items-start justify-start border border-wot-light-gray rounded-sm p-3">
+    <div className="w-full flex flex-col items-start justify-start border border-wot-light-gray rounded p-3">
       <div>
         <div className={`text-left w-fit font-bold ${priority.textColor}`}>
           {project.name}
         </div>
         <div className="text-left w-fit">{project.description}</div>
       </div>
-      <div className="w-full flex justify-start flex-wrap gap-6 mt-4">
-        <div className="w-fit min-w-fit text-left">
-          <div className={`${priority.textColor}`}>Task Completion</div>
-          {+project.totalTasks > 0 ? (
-            <div>
-              {`${project.completedTasks} of ${project.totalTasks} (` +
-                +project.completedTasks / +project.totalTasks +
-                "%)"}
-            </div>
-          ) : (
-            <div className="font-light italic">No Tasks Yet</div>
-          )}
+      <div className="w-full flex justify-start flex-wrap gap-x-6 gap-y-4 mt-4">
+        <div className="w-fit flex justify-start flex-wrap gap-x-6 gap-y-4">
+          <div className="w-fit text-left">
+            <div className={`${priority.textColor}`}>Created</div>
+            <div>{project.dateCreated.slice(0, 10)}</div>
+          </div>
+          <div className="w-fit text-left">
+            <div className={`${priority.textColor}`}>Due</div>
+            <div>{project.due.slice(0, 10)}</div>
+          </div>
         </div>
-        <div className="w-fit text-left">
-          <div className={`${priority.textColor}`}>Priority</div>
-          <div>{priority.string}</div>
+        <div className="w-fit flex justify-start flex-wrap gap-x-6 gap-y-4">
+          <div className="w-fit min-w-fit text-left">
+            <div className={`${priority.textColor}`}>Task Completion</div>
+            {+project.totalTasks > 0 ? (
+              <div>
+                {`${project.completedTasks} of ${project.totalTasks} (` +
+                  +project.completedTasks / +project.totalTasks +
+                  "%)"}
+              </div>
+            ) : (
+              <div className="font-light italic">No Tasks Yet</div>
+            )}
+          </div>
+          <div className="w-fit text-left">
+            <div className={`${priority.textColor}`}>Priority</div>
+            <div>{priority.string}</div>
+          </div>
         </div>
       </div>
     </div>
