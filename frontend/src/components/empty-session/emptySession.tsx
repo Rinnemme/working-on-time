8528cc333ecec6986b76ce70cedfa9e30b3ca0aa
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-01-31 17:37:56 -0500

    styling tweaks to empty session

--- frontend/src/components/empty-session/emptySession.tsx
+++ frontend/src/components/empty-session/emptySession.tsx
@@ -8,15 +8,15 @@ export default function EmptySession() {
   const projects = useSelector((state: AppState) => state.projects);
   const isLoading = useSelector((state: AppState) => state.isLoading);
   return (
-    <div className="bg-white fixed top-0 w-full h-full z-0 fade-in">
+    <div className="bg-white top-0 w-full h-full z-0 fade-in">
       <div className="relative flex items-start pt-12 pb-20 sm:pt-14 justify-center w-full bg-wot-off-white">
         <div className="text-center flex items-center w-full flex-col px-2">
-          <h1 className="text-2xl font-semibold text-wot-rose sm:text-3xl">
+          <h1 className="text-3xl font-bold text-wot-rose sm:text-3xl">
             New Working Session
           </h1>
-          <h3 className="text-md mt-3 mb-2 font-semibold text-wot-black">
-            Click a project to begin a working session
-          </h3>
+          <h2 className="font-bold text-3xl mt-2 mb-3 text-wot-yellow">
+            Select a Project
+          </h2>
           {isLoading && <Throbber />}
           {!isLoading && projects.length > 0 && (
             <SessionSelectList projects={projects} />
