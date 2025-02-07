258874bc440010a70c0f1555758e8b10725a83c5
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2025-02-07 12:33:52 -0500

    Update timer form language to reflect setting timer, not starting session (session starts paused)

--- frontend/src/components/working-session/timer-form/timerForm.tsx
+++ frontend/src/components/working-session/timer-form/timerForm.tsx
@@ -179,7 +179,7 @@ export default function TimerForm({ closeTimer }: { closeTimer: () => void }) {
               >
                 {timer.restingDuration && timer.workingDuration
                   ? "Save"
-                  : "Start Session"}
+                  : "Set Timer"}
               </button>
             </div>
           </div>
