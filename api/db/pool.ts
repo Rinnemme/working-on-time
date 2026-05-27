99399d58251e02511b26075c79e0d529e35af9b3
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2026-05-27 17:54:48 -0400

    add rejectUnauthorized: false to pool for local testing

--- api/db/pool.ts
+++ api/db/pool.ts
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 exports.pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
